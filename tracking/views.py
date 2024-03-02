9c029e699a8ad1be9031f68647a2ddd59deb0db6
Pavlo Starobub <starodub.pavlo@alsodev.md>
2024-03-03 00:55:28 +0200

    Updated with query params

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,4 @@
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,15 +6,30 @@ from tracking.models import Post
 from tracking.serializers import PostSerializer
 
 
-class PostListView(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
+# class PostListView(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#
+#         # Create a serializer instance for the list of posts with specific fields
+#         serializer = PostSerializer(
+#             instance=posts, many=True, fields=["title"]
+#         )
+#
+#         serialized_data = serializer.data
+#
+#         return Response(serialized_data)
 
-        # Create a serializer instance for the list of posts with specific fields
-        serializer = PostSerializer(
-            instance=posts, many=True, fields=["title"]
-        )
 
-        serialized_data = serializer.data
+# With this setup, you can make requests to /api/posts/ and include the fields parameter in the query string to dynamically choose the fields you want to include in the response. For example, /api/posts/?fields=title,content.
 
-        return Response(serialized_data)
+
+class DynamicFieldsModelViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        # Pass the request's query parameters to the serializer
+        fields = self.request.query_params.get("fields", None)
+        print(fields)
+        kwargs["context"] = self.get_serializer_context()
+        return self.serializer_class(fields=fields, *args, **kwargs)
