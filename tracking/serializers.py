b7bdfd79e941449c5f3964345f752538bf4f6cfe
Pavlo Starobub <starodub.pavlo@alsodev.md>
2024-03-03 14:58:37 +0200

    Updated with typehint

--- tracking/serializers.py
+++ tracking/serializers.py
@@ -3,19 +3,32 @@ from rest_framework import serializers
 from tracking.models import Post
 
 
+# class DynamicFieldsModelSerializerMixin(serializers.ModelSerializer):
+#     def __init__(self, *args: tuple, **kwargs: dict) -> None:
+#         fields = kwargs.pop("fields", None)
+#         if fields is not None:
+#             list_fields = fields.split(",")
+#         super(DynamicFieldsModelSerializerMixin, self).__init__(
+#             *args, **kwargs
+#         )
+#         if fields is not None:
+#             allowed = set(list_fields)
+#             existing = set(self.fields)
+#             for field_name in existing - allowed:
+#                 self.fields.pop(field_name)
+
+
 class DynamicFieldsModelSerializerMixin(serializers.ModelSerializer):
     def __init__(self, *args: tuple, **kwargs: dict) -> None:
         fields = kwargs.pop("fields", None)
         if fields is not None:
-            list_fields = fields.split(",")
+            list_fields = set(fields.split(","))
+            existing_fields = set(self.fields)
+            for field_name in existing_fields - list_fields:
+                self.fields.pop(field_name)
         super(DynamicFieldsModelSerializerMixin, self).__init__(
             *args, **kwargs
         )
-        if fields is not None:
-            allowed = set(list_fields)
-            existing = set(self.fields)
-            for field_name in existing - allowed:
-                self.fields.pop(field_name)
 
 
 class PostSerializer(
